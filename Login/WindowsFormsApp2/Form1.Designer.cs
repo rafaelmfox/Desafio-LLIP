2c3d66e6847314d1f91677eb6df2abde0c003ed0
Rafael <rafaelmfox@gmail.com>
2022-03-28 09:35:48 -0300

    Formulario de Login
    um formulario simples de login onde da algumas sugestoes caso o cliente ja esteje cadastrado

---
+++ Login/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblNascimento = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.txtNascimento = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(106, 80);
+            this.txtUsuario.MaxLength = 10;
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(351, 31);
+            this.txtUsuario.TabIndex = 0;
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogin.Location = new System.Drawing.Point(234, 47);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(53, 20);
+            this.lblLogin.TabIndex = 2;
+            this.lblLogin.Text = "Login";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Location = new System.Drawing.Point(29, 87);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(71, 20);
+            this.lblUsuario.TabIndex = 3;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // lblNascimento
+            // 
+            this.lblNascimento.AutoSize = true;
+            this.lblNascimento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNascimento.Location = new System.Drawing.Point(47, 124);
+            this.lblNascimento.Name = "lblNascimento";
+            this.lblNascimento.Size = new System.Drawing.Size(48, 20);
+            this.lblNascimento.TabIndex = 5;
+            this.lblNascimento.Text = "Data";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(106, 200);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(92, 48);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.Location = new System.Drawing.Point(215, 200);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(129, 48);
+            this.btnCadastrar.TabIndex = 4;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(365, 200);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(92, 48);
+            this.btnSair.TabIndex = 5;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(47, 161);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(53, 20);
+            this.lblEmail.TabIndex = 10;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(106, 154);
+            this.txtEmail.MaxLength = 20;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(351, 31);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.AutoSize = true;
+            this.lblMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensagem.Location = new System.Drawing.Point(102, 273);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(57, 20);
+            this.lblMensagem.TabIndex = 11;
+            this.lblMensagem.Text = "Aviso:";
+            // 
+            // txtNascimento
+            // 
+            this.txtNascimento.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNascimento.Location = new System.Drawing.Point(106, 117);
+            this.txtNascimento.Mask = "00/00/0000";
+            this.txtNascimento.Name = "txtNascimento";
+            this.txtNascimento.Size = new System.Drawing.Size(351, 31);
+            this.txtNascimento.TabIndex = 1;
+            this.txtNascimento.ValidatingType = typeof(System.DateTime);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 390);
+            this.Controls.Add(this.txtNascimento);
+            this.Controls.Add(this.lblMensagem);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblNascimento);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.txtUsuario);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblNascimento;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblMensagem;
+        private System.Windows.Forms.MaskedTextBox txtNascimento;
+    }
+}
+
---
+++ Login/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class frmPrincipal : Form
+    {
+        List<string> vCadastro = new List<string>();
+        
+        
+        public frmPrincipal()
+        {
+            InitializeComponent();
+
+            vCadastro.Add("USUARIO:admin");
+            vCadastro.Add("DATA:123");
+            vCadastro.Add("EMAIL:admin@admin.com");
+            vCadastro.Add("USUARIO:admin2");
+            vCadastro.Add("DATA:1232");
+            vCadastro.Add("EMAIL:admin@admin.com2");
+            vCadastro.Add("USUARIO:rafael");
+            vCadastro.Add("DATA:02/07/1991");
+            vCadastro.Add("EMAIL:rafael@rafael.com");
+
+            lblMensagem.Text = string.Empty;
+
+
+        }
+
+        
+        
+
+        /// 
+        /// Sair do Programa
+        /// 
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }//private void btnSair_Click(object sender, EventArgs e)
+
+        /// 
+        /// botao que faz o cadastro do cliente caso ele nao tenha e faz a checagem se ele ja e cadastrado
+        /// Sugeriri um novo usuario 
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+
+            if (ChecarUsuarioJaCadastrado(txtUsuario.Text))
+            {
+                string aux;
+                List<string> lista = new List<string>();
+                int contador = 0;
+
+                lblMensagem.ForeColor = System.Drawing.Color.Red;
+                lblMensagem.Text = "Esse usuario ja foi cadastrado \n usuarios disponiveis";
+
+                while (contador < 3)
+                {
+                    aux = SugestaoNomeUsuario();
+
+                    if (lista.Contains(aux))
+                    {
+                        continue;
+                    }// if (lista.Contains(aux))
+                    lista.Add(aux);
+                    contador++;
+                }//while (contador < 3)
+
+                for (int i = 0; i < 3; i++)
+                {
+                    lblMensagem.Text = lblMensagem.Text + "\n"+lista[i];
+                }//for (int i = 0; i < 3; i++)
+
+
+            }
+            else
+            {
+                vCadastro.Add("USUARIO:"+txtUsuario.Text);
+                vCadastro.Add("DATA:"+txtNascimento.Text);
+                vCadastro.Add("EMAIL:"+txtEmail.Text);
+            }//if (ChecarUsuarioJaCadastrado(txtUsuario.Text))
+
+
+        }//private void btnCadastrar_Click(object sender, EventArgs e)
+
+
+        /// 
+        /// botao de login , ele ira checar se todas as info do usuario esta correta
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (Login(txtUsuario.Text,txtNascimento.Text,txtEmail.Text))
+            {
+                lblMensagem.ForeColor = System.Drawing.Color.Green;
+                lblMensagem.Text = "vc esta logado";
+            }
+        }//private void btnLogin_Click(object sender, EventArgs e)
+
+
+
+        /// verifica se o usuario ja esta cadastrado para ser feito o login
+        /// <param name="usuario">nome do usuario</param>
+        /// <param name="data">data de nascimento</param>
+        /// <param name="email">email do usuario</param>
+        /// <returns>true / false</returns>
+        public bool Login(string usuario, string data, string email)
+        {
+            for (int i = 0; i < vCadastro.Count; i++)
+            {
+                if ((vCadastro[i] == "USUARIO:" + usuario) && (vCadastro[i+1] == "DATA:" + data) && (vCadastro[i+2] == "EMAIL:"+email))
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }//public bool Login(string usuario, string data, string email)
+
+        ///Verifica se o usuario ja existe
+        /// <param name="usuario">nome do usuario que sera cadastrado</param>
+        /// <returns>true / false</returns>
+        public bool ChecarUsuarioJaCadastrado(string usuario)
+        {
+            if (vCadastro.Contains("USUARIO:" + usuario))
+            {
+                return true;
+            }
+
+            return false;
+
+        }//public bool ChecarUsuarioJaCadastrado(string usuario)
+
+
+        /// 
+        /// Ira montar um nome usando o nome de usuario 
+        /// junto do email e com um numero aleatorio entre 100 a 999
+        /// ira ter um randon onde ele ira montar aleatoriamentede 0 a 3
+        /// 0 = nome usuario + email + numero aletorio 100 a 999
+        /// 1 = nome usuario + data 4 primeiros digitos= email
+        /// 2 = nome do usuario = email = ano de nascimento
+        /// 3 = nome usuario + email 3 primeiros digitos + numero de 100 a 999
+        public string SugestaoNomeUsuario()
+        {
+            Random rnd = new Random();
+            var tipoSugestao = rnd.Next(0, 3);
+
+            if (tipoSugestao == 0)
+            {
+                return txtUsuario.Text + desmenbrarEmail(txtEmail.Text) + rnd.Next(100, 999);
+            }
+            if (tipoSugestao == 1)
+            {
+
+                return txtUsuario.Text + txtNascimento.Text.Replace("/", "").Substring(0, 4)+ desmenbrarEmail(txtEmail.Text);
+
+            }
+            if (tipoSugestao == 2)
+            {
+                string data = txtNascimento.Text.Replace("/", "");
+                data = data.Replace(data.Substring(0, 4), "");
+                return txtUsuario.Text + desmenbrarEmail(txtEmail.Text).Substring(0,3)+data;
+            }
+            if (tipoSugestao == 3)
+            {
+                return txtUsuario.Text + desmenbrarEmail(txtEmail.Text).Substring(0, 3) + rnd.Next(100, 999);
+            }
+
+
+
+
+            return txtUsuario.Text + desmenbrarEmail(txtEmail.Text) + rnd.Next(100,999);
+        }//public string SugestaoNomeUsuario()
+
+
+        /// <summary>
+        /// Desmenbra o email para fazer o join depois na função SugestaoNomeUsuario()
+        /// </summary>
+        /// <param name="email">a variavel onde ira receber o email</param>
+        /// <returns>string</returns>
+        public string desmenbrarEmail(string email)
+        { 
+            int aux = 0;
+
+            //controle para ver se esta Ok com o email caso contrario retorna ""
+            if ((email == "") || (email == null) || (email.Contains(" ")))
+            {
+                return "";
+            }
+            else
+            {
+                aux = email.IndexOf("@");
+                email = email.Substring(0, aux);
+            }
+
+            if (email.Contains("."))
+            {
+                aux = email.IndexOf(".");
+                return email.Substring(0, aux);
+            }
+
+            if (email.Contains("-"))
+            {
+                aux = email.IndexOf(".");
+                return email.Substring(0, aux);
+            }
+
+            if (email.Contains("_"))
+            {
+                aux = email.IndexOf(".");
+                return email.Substring(0, aux);
+            }
+
+            if (email.Length >= 5)
+            {
+                return email.Substring(0, 5);
+            }
+            else
+            {
+                return email.Substring(0);
+            }
+
+
+            return "";
+
+
+
+
+        }//public string desmenbrarEmail(string email)
+
+
+
+
+
+
+
+
+
+
+    }//public partial class frmPrincipal : Form
+}//namespace WindowsFormsApp2
